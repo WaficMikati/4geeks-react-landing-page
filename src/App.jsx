f6307e0353d73e5695b7c94dbd173824466169e4
WaficMikati <wrmikati@proton.me>
2025-08-21 04:18:35 -0500

    Cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,27 @@
-import { useState } from 'react'
-import { Navbar } from './components/Navbar.jsx'
-import { Content } from './components/Content.jsx'
-import albumsEn from './data/en/albums.json'
-import albumsEs from './data/es/albums.json'
-import siteContentEn from './data/en/siteContent.json'
-import siteContentEs from './data/es/siteContent.json'
-import './styles/App.css'
+import { useState } from "react";
+import { Navbar } from "./components/Navbar.jsx";
+import { Content } from "./components/Content.jsx";
+import albumsEn from "./data/en/albums.json";
+import albumsEs from "./data/es/albums.json";
+import siteContentEn from "./data/en/siteContent.json";
+import siteContentEs from "./data/es/siteContent.json";
+import "./styles/App.css";
 
 function App() {
-  const [language, setLanguage] = useState('en');
+  const [language, setLanguage] = useState("en");
 
   const allData = {
     en: { albums: albumsEn, siteContent: siteContentEn },
-    es: { albums: albumsEs, siteContent: siteContentEs }
-  }
+    es: { albums: albumsEs, siteContent: siteContentEs },
+  };
 
-  const { albums, siteContent } = allData[language]
+  const { albums, siteContent } = allData[language];
 
   return (
     <div className="d-grid vh-100">
-      <Navbar ref={albums} lang={language} changeLanguage={setLanguage}/>
-      <Content albums={albums} content={siteContent}/>
+      <Navbar ref={albums} lang={language} changeLanguage={setLanguage} />
+      <Content albums={albums} content={siteContent} />
     </div>
-  )
+  );
 }
-export default App
+export default App;
