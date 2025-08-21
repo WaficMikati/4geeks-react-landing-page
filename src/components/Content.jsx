ddadcd5adb2c9682cfad01c7996f8dc33accdd4f
WaficMikati <wrmikati@proton.me>
2025-08-21 01:57:03 -0500

    Decided to center text below sm breakpoint

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -8,7 +8,7 @@ export function Content(props) {
       {Object.entries(props.albums).map(([region, albumList]) => (
         <div key={"region"+ region} className='text-white mb-5' id={region}>
           <div className='pt-1 rounded-5 tbg'>
-            <h1 className='text-end display-2 fw-bold py-5 pe-4 pe-sm-5 region-title'>{region}</h1>
+            <h1 className='text-center text-sm-end display-2 fw-bold py-5 pe-sm-5 region-title'>{region}</h1>
             <div className="row justify-content-center g-4">
               {Object.entries(albumList).map(([id, album]) => (
                 album.url === "" ? "" :
